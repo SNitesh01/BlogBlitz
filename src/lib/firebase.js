b094e9e52a85e4983cefde1f09ee4c7fa863c033
SNitesh01 <itskynitesh@gmail.com>
2024-01-17 00:07:52 +0530

    changes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZtBxlS8mMNHKqvwOEmXTUggPPXQqfM-o",
-  authDomain: "blog-app-d99e6.firebaseapp.com",
-  projectId: "blog-app-d99e6",
-  storageBucket: "blog-app-d99e6.appspot.com",
-  messagingSenderId: "900968402662",
-  appId: "1:900968402662:web:dbd0b50f50535df81b3527",
-  measurementId: "G-T4SN82TMHM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURE_ID
 };
 
 
